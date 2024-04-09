224795f7fd371582c762e1d553a918c65d1d4819
FegrusB <fergus@beckerleg.co.uk>
2024-04-09 16:08:53 +0100

    Removed 2d array, fails on time

--- src/main/java/leet/code/ArraysStrings/IncreasingTripletSubsequence.java
+++ src/main/java/leet/code/ArraysStrings/IncreasingTripletSubsequence.java
@@ -4,39 +4,36 @@ public class IncreasingTripletSubsequence{
 
 
     public static void main(String[] args) {
-        System.out.println(increasingTriplet(new int[] {2,1,5,0,4,6}));
+        System.out.println(increasingTriplet(new int[] {5,4,3,2,1}));
     }
 
     public static boolean increasingTriplet(int[] nums) {
 
-        boolean[][] bigger = new boolean[nums.length][nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] < nums[j]){bigger[i][j] = true;}
-            }
-        }
+        int biggest = nums[0];
+        for (int i: nums){if(biggest < i){biggest = i;}}
 
         int i = 0;
         boolean possible = false;
         while (!possible && i < nums.length){
-            possible = checkPath(bigger,i,0);
+            possible = checkPath(nums,i,0);
             i++;
         }
-
         return possible;
     }
 
-    public static boolean checkPath(boolean[][] bigger, int x, Integer level){
+    public static boolean checkPath(int[] nums, int x, Integer level, int biggest){
 
         boolean possible = false;
+
 
         level++;
         if (level == 3){ return true;}
 
-        int i = 0;
-        while (!possible && i < bigger.length){
-            if (bigger[x][i]){possible = checkPath(bigger,i,level);}
+        int i = x + 1;
+        while (!possible && i < nums.length){
+            if (nums[x] < nums[i]){
+                possible = checkPath(nums,i,level);
+            }
             i++;
         }
         return possible;
